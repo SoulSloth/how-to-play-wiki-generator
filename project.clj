65b54ecb070df2976147ff50ed296628f9c2a58c
SoulSloth <root@DESKTOP-EKBQMB8.localdomain>
2022-02-09 20:39:20 -0500

    Leave unlicensed(full copyright) for now

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject how-to-play-wiki "0.1.0-SNAPSHOT"
   :description "Simple wiki static site generator for gaming websites"
-  :license {:name "Attribution 3.0 Unported (CC BY 3.0)"
-            :url "https://creativecommons.org/licenses/by/3.0/legalcode"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [stasis/stasis "2.5.1"]
                  [ring "1.9.5"]
